ea19b3b25ee47bebeb69c3662e5a3ee9d7b13d65
Heman <zack.hehmann@gmail.com>
2018-08-28 14:36:13 -0500

    Trying to print string text and variable text on same line with 1 command.

--- fan_pwm/fan_pwm.ino
+++ fan_pwm/fan_pwm.ino
@@ -1,12 +1,19 @@
 const int fan1_pin6 = 6;
-
+int fan_speed = 20;
 
 void setup() {
   // put your setup code here, to run once:
+
+Serial.begin(115200);  //initilize serial connection
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-analogWrite(fan1_pin6, 20);
+  if(Serial.available())
+  {
+    Serial.println("About to turn on fan" + fan1_pin6);
+    analogWrite(fan1_pin6, 20);
+  }
 }
