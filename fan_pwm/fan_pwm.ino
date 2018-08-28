bb9f2d44d18eec99433e589b4125c9efdab5fbb1
Heman <zack.hehmann@gmail.com>
2018-08-28 14:02:03 -0500

    First Version

---
+++ fan_pwm/fan_pwm.ino
@@ -0,0 +1,12 @@
+const int fan1_pin6 = 6;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(fan1_pin6, 20);
+}
